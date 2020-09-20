b908ea5221b9728436283abae1a1b7365edfeaea
Łukasz Komodziński <komoda102@gmail.com>
2020-09-20 16:21:17 +0200

    Training session changes

---
+++ SamuraiApp/ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static SamuraiContext context = new SamuraiContext(); 
+
+        static void Main(string[] args)
+        {
+            //This is not good practice to use this to create database
+            //It is just for testing purposes
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            context.Database.EnsureDeleted();
+            Console.WriteLine("Press Any key...");
+            Console.ReadKey();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai {Name = "Komoda San"};
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+    }
+}
--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -8,5 +8,15 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (local)\\SQLEXPRESS; Initial Catalog = SamuraiAppData; Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new {s.SamuraiId, s.BattleId});
+        }
     }
 }
