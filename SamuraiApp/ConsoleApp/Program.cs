882e5b56d9fa5b2585a732e4549e6e8cb6e8f3bd
Łukasz Komodziński <komoda102@gmail.com>
2020-10-13 22:03:33 +0200

    .

--- SamuraiApp/ConsoleApp/Program.cs
+++ SamuraiApp/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 
@@ -13,13 +14,40 @@ namespace ConsoleApp
         {
             //This is not good practice to use this to create database
             //It is just for testing purposes
-            context.Database.EnsureCreated();
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
-            context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+            //GetSamurais("Before Add:");
+            //AddMultipleSamurais();
+            //AddMultiptleTypes();
+            //AddSamurai();
+            //GetSamurais("After Add:");
+            
+            //context.Database.EnsureDeleted();
             Console.WriteLine("Press Any key...");
             Console.ReadKey();
+        }
+
+
+
+        private static void AddMultiptleTypes()
+        {
+            var sam1 = new Samurai(){Name = "New1"};
+            var clan = new Clan() {ClanName = "New Clan"};
+
+            context.AddRange(sam1, clan);
+
+            context.SaveChanges();
+        }
+
+        private static void AddMultipleSamurais()
+        {
+            var sam1 = new Samurai() {Name = "New1"};
+            var sam2 = new Samurai() {Name = "New2"};
+            var sam3 = new Samurai() {Name = "New3"};
+            var sam4 = new Samurai() {Name = "New4"};
+
+            context.Samurais.AddRange(sam1, sam2);
+
+            context.SaveChanges();
         }
 
         private static void GetSamurais(string text)
@@ -37,6 +65,24 @@ namespace ConsoleApp
             var samurai = new Samurai {Name = "Komoda San"};
             context.Samurais.Add(samurai);
             context.SaveChanges();
+            
         }
+
+        //private static void GetSamurais2()
+        //{
+        //    var samurais = context.Samurais.Where(s=>EF.).ToList();
+        //}
+
+        private static void QueryAndUpdateBattle_Disconnecte()
+        {
+            var battle = context.Battles.AsNoTracking().FirstOrDefault();
+            battle.EndDate = new DateTime(1560, 06, 30);
+            using (var newContextInstance = new SamuraiContext())
+            {
+                newContextInstance.Battles.Update(battle);
+                newContextInstance.SaveChanges();
+            }
+        }
+
     }
 }
--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
@@ -8,15 +9,31 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        //Show only database commands
+                        category == DbLoggerCategory.Database.Command.Name
+                        //Show only basic information
+                        && level == LogLevel.Information)
+                    .AddConsole();
+            });
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source = (local)\\SQLEXPRESS; Initial Catalog = SamuraiAppData; Trusted_Connection=True;");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory).EnableSensitiveDataLogging()
+                .UseSqlServer("Data Source = (local)\\SQLEXPRESS; Initial Catalog = SamuraiAppData; Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new {s.SamuraiId, s.BattleId});
+            modelBuilder.Entity<Horse>().ToTable("Horses");
         }
     }
 }
